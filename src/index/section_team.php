86a3980fbd13df281656d012aefb7dde5355f016
Sir Felipe <felipe.dioliveira@outlook.com>
2019-05-25 17:04:42 -0300

    Adding the coolest member on the team, because i forgot myself...

--- src/index/section_team.php
+++ src/index/section_team.php
@@ -3,14 +3,14 @@
   <div class="container">
     <h2 class="mb-5">Nosso time</h2>
     <div class="row">
-      <div class="col-md-3">
+      <div class="col-md-4">
         <div class="testimonial-item mx-auto mb-5 mb-lg-0">
           <img class="img-fluid rounded-circle mb-3" src="assets/img/carol_ina.jpg" alt="">
           <h5>Carolina</h5>
           <p class="font-weight-light mb-0">A Braba</p>
         </div>
       </div>
-      <div class="col-md-3">
+      <div class="col-md-4">
         <div class="testimonial-item mx-auto mb-5 mb-lg-0">
           <img class="img-fluid rounded-circle mb-3" src="assets/img/yuri_zeira.jpg" alt="">
           <h5>Yuri</h5>
@@ -24,13 +24,25 @@
           <p class="font-weight-light mb-0">Falta muito!</p>
         </div>
       </div>
-      <div class="col-md-3">
-        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-          <img class="img-fluid rounded-circle mb-3" src="assets/img/gabr_iel.jpg" alt="">
-          <h5>Gabriel</h5>
-          <p class="font-weight-light mb-0">Comprei um pokémon e ele veio estragado!</p>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="img-break testimonial-item mx-auto mb-5 mb-lg-0">
+              <img class="img-fluid rounded-circle mb-3" src="assets/img/gabr_iel.jpg" alt="">
+              <h5>Gabriel</h5>
+              <p class="font-weight-light mb-0">Comprei um pokémon e ele veio estragado!</p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="img-break testimonial-item mx-auto mb-5 mb-lg-0">
+              <img class="img-fluid rounded-circle mb-3" src="assets/img/fe_lipe.jpeg" alt="">
+              <h5>Felipe</h5>
+              <p class="font-weight-light mb-0">Aleatoriedade em pessoa</p>
+            </div>
+          </div>
         </div>
       </div>
+
     </div>
   </div>
 </section>
